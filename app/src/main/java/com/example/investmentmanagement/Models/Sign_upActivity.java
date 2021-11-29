a9ddd926aec7b35b4321b35f769c2250619834e3
Yuhao Wang <70633495+Yuhao-1999@users.noreply.github.com>
2021-11-29 20:01:22 +0100

    Sign up view

---
+++ app/src/main/java/com/example/investmentmanagement/Models/Sign_upActivity.java
@@ -0,0 +1,20 @@
+package com.example.investmentmanagement.Models;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.investmentmanagement.R;
+
+public class Sign_upActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+
+
+    }
+}
