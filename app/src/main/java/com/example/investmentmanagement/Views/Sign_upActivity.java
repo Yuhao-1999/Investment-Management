a5ce865d95d13b36043c5b41e199188fd9102d71
Yuhao Wang <70633495+Yuhao-1999@users.noreply.github.com>
2021-11-29 21:15:33 +0100

    create Sign_upActivity

--- app/src/main/java/com/example/investmentmanagement/Views/Sign_upActivity.java
+++ app/src/main/java/com/example/investmentmanagement/Views/Sign_upActivity.java
@@ -1,14 +1,13 @@
-package com.example.investmentmanagement.Models;
+package com.example.investmentmanagement.Views;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.investmentmanagement.R;
 
 public class Sign_upActivity extends AppCompatActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
