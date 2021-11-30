d85f5f28761672ce6b302fac82bcd3cca250c7ea
Satish627 <Satish.grg627@gmail.com>
2021-11-30 16:01:33 +0100

    Added a navigation drawer

--- app/src/main/java/com/example/investmentmanagement/MainActivity.java
+++ app/src/main/java/com/example/investmentmanagement/MainActivity.java
@@ -1,14 +1,13 @@
 package com.example.investmentmanagement;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+        setContentView(R.layout.login);
+    }}
