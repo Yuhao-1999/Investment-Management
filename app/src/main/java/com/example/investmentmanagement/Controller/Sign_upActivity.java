5efce6f82611a36391d49868d33d8277b51b43a3
Yuhao Wang <70633495+Yuhao-1999@users.noreply.github.com>
2021-11-30 19:13:53 +0100

    Login and signup activity onclick

---
+++ app/src/main/java/com/example/investmentmanagement/Controller/Sign_upActivity.java
@@ -0,0 +1,52 @@
+package com.example.investmentmanagement.Controller;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.investmentmanagement.MainActivity;
+import com.example.investmentmanagement.R;
+
+public class Sign_upActivity extends AppCompatActivity{
+    TextView sign_upTitle,enter_email,enter_username,enter_password,initial_budget,already;
+    EditText editEmail,editUsername,editPassword,editNumber;
+  private   Button sign_upButton,log_inButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+
+        sign_upTitle = (TextView) findViewById(R.id.sign_upTitle);
+        enter_email= (TextView) findViewById(R.id.enter_email);
+        enter_username= (TextView) findViewById(R.id.enter_username);
+        enter_password = (TextView) findViewById(R.id.enter_password);
+        initial_budget = (TextView) findViewById(R.id.initial_budget);
+        already = (TextView) findViewById(R.id.already);
+
+        editEmail = (EditText) findViewById(R.id.editEmailAddress);
+        editUsername = (EditText) findViewById(R.id.editUsername);
+        editPassword = (EditText) findViewById(R.id.editPassword);
+        editNumber = (EditText) findViewById(R.id.editNumber);
+
+        sign_upButton  = (Button) findViewById(R.id.sign_upButton);
+        log_inButton = (Button) findViewById(R.id.log_inButton);
+
+        log_inButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Sign_upActivity.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
+
+
--- app/src/main/java/com/example/investmentmanagement/MainActivity.java
+++ app/src/main/java/com/example/investmentmanagement/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.investmentmanagement;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.investmentmanagement.Controller.Sign_upActivity;
 
+public class MainActivity extends AppCompatActivity{
+TextView textView,textView2,textView3,textView4;
+EditText editTextTextEmailAddress,editTextTextPassword;
+private Button button,button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-    }}
+
+        textView = (TextView) findViewById(R.id.textView);
+        textView2 = (TextView) findViewById(R.id.textView2);
+        textView3 = (TextView) findViewById(R.id.textView3);
+        textView4= (TextView) findViewById(R.id.textView4);
+
+        editTextTextEmailAddress =(EditText) findViewById(R.id.editTextTextEmailAddress);
+        editTextTextPassword =(EditText) findViewById(R.id.editTextTextPassword);
+
+        button = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent= new Intent(MainActivity.this,Sign_upActivity.class);
+            startActivity(intent);
+
+        }
+    });
+}
+}
+
